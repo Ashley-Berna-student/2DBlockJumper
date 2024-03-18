c255573827757032fa27192dedca6b1ac005f9bf
Ashley <144158076+Ashley-Berna-student@users.noreply.github.com>
2024-03-18 18:54:03 -0400

    adding to obstacle script

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float initialSpeed = 10.0f;
+    public float maxSpeed = 20.0f;
+    public float speedIncreaseAmount = 2.0f;
+    private float speed;
 
     public float end = 1.0f;
     private deleter deleterScript;
@@ -11,14 +14,13 @@ public class Obstacle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         deleterScript = GetComponent<deleter>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-        // Make sure this calculation is frame rate independent (hint: use Time.deltaTime)
+    {        // Make sure this calculation is frame rate independent (hint: use Time.deltaTime)
 
 
         // TODO: If the obstalce is off screen to the left, destroy this GameObject (hint: Destroy(gameObject))
@@ -27,14 +29,23 @@ public class Obstacle : MonoBehaviour
             Destroy(gameObject);
         }
 
-        if (deleterScript != null)
+        if (deleterScript != null && deleterScript.scoreCount >= 3)
         {
-            int scoreCount = deleterScript.scoreCount;
-            Debug.Log("Score count " + scoreCount);
-            if (scoreCount >= 3)
-            {
-                speed = 20.0f;
-            }
+            IncreaseSpeed();
         }
+
+        MoveObstacle();
     }
+
+
+        void IncreaseSpeed()
+        {
+            speed += speedIncreaseAmount * Time.deltaTime;
+            speed = Mathf.Min(speed, maxSpeed);
+        }
+
+        void MoveObstacle()
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
 }
